841d87008f375311bc70bbde16ce93203a8ef41a
William Baffour <nanabaffour6@gmail.com>
2026-09-14 21:33:11 +0000

    Enable Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 
 import App from './App';
 import '../app/globals.css';
@@ -7,5 +8,6 @@ import '../app/globals.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+<Analytics />
   </StrictMode>,
 );
